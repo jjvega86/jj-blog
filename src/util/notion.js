c107b8939c100a0e3746de1484a6eade6d89432c
JJ <jj.vega86@gmail.com>
2022-11-18 07:19:19 -0500

    📦 NEW: Add getPostContent for page content

--- src/util/notion.js
+++ src/util/notion.js
@@ -2,12 +2,24 @@ import { Client } from "@notionhq/client";
 const notion = new Client({ auth: import.meta.env.NOTION_KEY });
 const databaseId = import.meta.env.NOTION_DATABASE_ID;
 
-export default async function getPosts() {
+export async function getPosts() {
   try {
     const response = await notion.databases.query({
       database_id: databaseId,
     });
-    console.log(response);
+    return response.results;
+  } catch (error) {
+    console.error(error.body);
+  }
+}
+
+export async function getPostContent(pageId) {
+  try {
+    const response = await notion.blocks.children.list({
+      block_id: pageId,
+      page_size: 50,
+    });
+    return response.results;
   } catch (error) {
     console.error(error.body);
   }
